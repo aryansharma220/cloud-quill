317a0bfee107082fa3622ed4ae35a1a3d2aa1367
aryansharma220 <aryansharma220318@gmail.com>
2024-12-05 01:13:31 +0530

    working on online editor

---
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -0,0 +1,36 @@
+import { cn } from "@/lib/utils"
+import { LucideIcon } from "lucide-react"
+
+
+interface ToolbarButtonProps {
+  onClick?: () => void
+  isActive?: boolean
+  icon: LucideIcon
+}
+
+
+const ToolbarButton = ({
+  onClick,
+  isActive,
+  icon: Icon
+}: ToolbarButtonProps) => {
+  return (
+    <button 
+      onClick={onClick}
+      className={cn(
+        "text-sm h-7 min-w-7 flex items-center justify-center rounded-sm hover:bg-neutral-200/80",
+        isActive && "bg-neutral-200/80"
+      )}
+    >
+      <Icon className="size-4"/>
+    </button>
+  )
+}
+
+export const Toolbar = () => {
+  return (
+  <div className="bg-[#f1f4f9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
+    Toolbar
+  </div>
+  )
+}
---
+++ src/app/documents/page.tsx
@@ -0,0 +1,8 @@
+const DocumentsPage = () => {
+  return (
+  <h1>
+    Documents
+  </h1>
+  )
+}
+export default DocumentsPage
